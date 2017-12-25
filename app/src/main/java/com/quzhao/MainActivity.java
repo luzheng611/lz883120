130c505c56dad8709944ef50437c5578c4afe441
luzheng611 <luzheng611@gmail.com>
2017-12-26 01:10:52 +0800

    "delete useless library"

--- app/src/main/java/com/quzhao/MainActivity.java
+++ app/src/main/java/com/quzhao/MainActivity.java
@@ -1,15 +1,15 @@
 package com.quzhao;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        ViewPager viewPager =findViewById(R.id.viewpager_main);
     }
 
     public void  initMain(){
