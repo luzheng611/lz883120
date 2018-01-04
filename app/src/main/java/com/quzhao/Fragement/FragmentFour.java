c54f02fda14c2be2b52e7118ba4fe6777ea25cf2
luzheng611 <luzheng611@gmail.com>
2018-01-05 01:28:23 +0800

    "1/4 1:08"

--- app/src/main/java/com/quzhao/Fragement/FragmentFour.java
+++ app/src/main/java/com/quzhao/Fragement/FragmentFour.java
@@ -1,12 +1,18 @@
 package com.quzhao.Fragement;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.quzhao.R;
+import com.quzhao.Util.IUtils;
 
 /**
  * Created by Administrator on 2017/12/29.
@@ -14,11 +20,19 @@ import android.view.ViewGroup;
  */
 
 public class FragmentFour extends Fragment {
-
+    private View view;
+    private Drawable next;
+    private TextView invite;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        view=inflater.inflate(R.layout.fragmeny_mine,null);
+        invite=view.findViewById(R.id.invite);
+        next= ContextCompat.getDrawable(getActivity(),R.drawable.next);
+        int dp= IUtils.dip2px(getActivity(),10);
+        next.setBounds(0,0,dp,dp*next.getIntrinsicHeight()/next.getIntrinsicWidth());
+        invite.setCompoundDrawables(null,null,next,null);
+        return view;
 
     }
 }
--- app/src/main/java/com/quzhao/Fragement/FragmentThree.java
+++ app/src/main/java/com/quzhao/Fragement/FragmentThree.java
@@ -4,9 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.chad.library.adapter.base.entity.MultiItemEntity;
+import com.quzhao.R;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/12/29.
@@ -14,11 +23,35 @@ import android.view.ViewGroup;
  */
 
 public class FragmentThree extends Fragment {
-
+    private LinearLayout headView ;
+    private View view;
+    private RecyclerView recyclerView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_huati,null);
+        recyclerView=view.findViewById(R.id.recyclerView);
+
+
         return super.onCreateView(inflater, container, savedInstanceState);
 
+    }
+
+
+    private class  HuaTiAdapter extends BaseMultiItemQuickAdapter<MultiItemEntity,BaseViewHolder> {
+        /**
+         * Same as QuickAdapter#QuickAdapter(Context,int) but with
+         * some initialization data.
+         *
+         * @param data A new list is created out of this one to avoid mutable list
+         */
+        public HuaTiAdapter(List<MultiItemEntity> data) {
+            super(data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, MultiItemEntity item) {
+
+        }
     }
 }
